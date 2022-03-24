8aedc047c777c9e6120b1ebdd9dfd60ccf83989f
ifurther <55025025+ifurther@users.noreply.github.com>
2022-03-24 17:36:39 +0800

    update R script

--- install.R
+++ install.R
@@ -1,11 +1,14 @@
 #!/usr/bin/env Rscript
 
 # set a CRAN mirror
- local({r <- getOption("repos")
-       r["CRAN"] <- "https://cloud.r-project.org/"
-       options(repos=r)})
+localcran <- function() {
+    r <- getOption("repos")
+    r["CRAN"] <- "https://cloud.r-project.org/"
+    options(repos=r)
+    rm(r)
+} 
 
-local()
+localcran()
 
 install(c("IRkernel", "ggplot2", "dplyr", "tinytex", "reticulate"), lib="/usr/local/lib/R/site-library")
 IRkernel::installspec()
